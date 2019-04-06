e6bd5a798a915427c151fa80b36cafe0965092d1
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-04-06 14:18:17 -0400

    Register Route and Components Created

--- src/agent.js
+++ src/agent.js
@@ -2,6 +2,7 @@
 
 import superagentPromise from 'superagent-promise';
 import _superagent from 'superagent';
+import { request } from 'https';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
@@ -32,7 +33,9 @@ const Auth = {
   current: () =>
     requests.get('/user'),
   login: (email, password) =>
-    requests.post('/users/login', { user: { email, password } })
+    requests.post('/users/login', { user: { email, password } }),
+  register: (username, email, password) => 
+    requests.post('/users', {user: { username, email, password } })
 };
 
 export default {
